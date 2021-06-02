d45740fa17e723096cbbff694eeee01902d981d1
kaspernj <k@spernj.org>
2021-06-02 16:07:43 +0200

    Indicate clearly which variables are meant for usage outside the lib

--- src/readers-writer-lock.cjs
+++ src/readers-writer-lock.cjs
@@ -1,53 +1,53 @@
 module.exports = class ReadersWriterLock {
   constructor(args = {}) {
-    this.debug = args.debug
-    this.readers = 0
-    this.writers = 0
-    this.processQueueTimeout = null
-    this.readQueue = []
-    this.writeQueue = []
+    this._debug = args.debug
+    this._readers = 0
+    this._writers = 0
+    this._processQueueTimeout = null
+    this._readQueue = []
+    this._writeQueue = []
   }
 
   read(callback) {
-    if (this.debug) this._log("read")
+    if (this._debug) this._log("read")
     return new Promise((resolve, reject) => {
       this._readWithResolve({callback, resolve, reject})
     })
   }
 
   write(callback) {
-    if (this.debug) this._log("write")
+    if (this._debug) this._log("write")
     return new Promise((resolve, reject) => {
       this._writeWithResolve({callback, resolve, reject})
     })
   }
 
   _log(message) {
-    console.log("ReadersWriteLock", message, JSON.stringify({readers: this.readers, writers: this.writers}))
+    console.log("ReadersWriteLock", message, JSON.stringify({readers: this._readers, writers: this._writers}))
   }
 
   _readWithResolve(item) {
-    if (this.writers > 0) {
-      if (this.debug) this._log("Queue read")
-      this.readQueue.push(item)
+    if (this._writers > 0) {
+      if (this._debug) this._log("Queue read")
+      this._readQueue.push(item)
     } else {
-      if (this.debug) this._log("Process read")
+      if (this._debug) this._log("Process read")
       this._processRead(item)
     }
   }
 
   _writeWithResolve(item) {
-    if (this.readers > 0 || this.writers > 0) {
-      if (this.debug) this._log("Queue write")
-      this.writeQueue.push(item)
+    if (this._readers > 0 || this._writers > 0) {
+      if (this._debug) this._log("Queue write")
+      this._writeQueue.push(item)
     } else {
-      if (this.debug) this._log("Process write")
+      if (this._debug) this._log("Process write")
       this._processWrite(item)
     }
   }
 
   async _processRead(item) {
-    this.readers++
+    this._readers++
 
     try {
       await item.callback()
@@ -55,13 +55,13 @@ module.exports = class ReadersWriterLock {
     } catch (error) {
       item.reject(error)
     } finally {
-      this.readers--
+      this._readers--
       this._processQueueLater()
     }
   }
 
   async _processWrite(item) {
-    this.writers++
+    this._writers++
 
     try {
       await item.callback()
@@ -69,33 +69,33 @@ module.exports = class ReadersWriterLock {
     } catch (error) {
       item.reject(error)
     } finally {
-      this.writers--
+      this._writers--
       this._processQueueLater()
     }
   }
 
   _processQueueLater() {
-    if (this.processQueueTimeout) {
-      clearTimeout(this.processQueueTimeout)
+    if (this._processQueueTimeout) {
+      clearTimeout(this._processQueueTimeout)
     }
 
-    this.processQueueTimeout = setTimeout(() => this._processQueue(), 0)
+    this._processQueueTimeout = setTimeout(() => this._processQueue(), 0)
   }
 
   _processQueue() {
-    if (this.writers == 0) {
+    if (this._writers == 0) {
       // If no one has begun writing, we should try and proceed to next read item if any
-      const readQueueItem = this.readQueue.shift()
+      const readQueueItem = this._readQueue.shift()
 
       if (readQueueItem) {
-        if (this.debug) this._log("Process next read")
+        if (this._debug) this._log("Process next read")
         this._processRead(readQueueItem)
-      } else if (this.readers == 0) {
+      } else if (this._readers == 0) {
         // No writers, no next item to read - we should try and proceed to next write item if any
-        const writeQueueItem = this.writeQueue.shift()
+        const writeQueueItem = this._writeQueue.shift()
 
         if (writeQueueItem) {
-          if (this.debug) this._log("Process next write")
+          if (this._debug) this._log("Process next write")
           this._processWrite(writeQueueItem)
         }
       }
